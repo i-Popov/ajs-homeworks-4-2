4b0eb2119d82f2f174019e45f80f3a9ffa6ae27c
Igor Popov <reddym183@gmail.com>
2022-05-11 17:13:58 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import lifeLevel from './basic';
+// eslint-disable-next-line
+console.log('worked');
+// eslint-disable-next-line
+console.log(lifeLevel([
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+]));
